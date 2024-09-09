7d9fbae9a5b1f9e881446a03a4b7f4ce759596d6
Aaron Yee <Saber_53@yahoo.com>
2024-09-08 22:17:09 -1000

    initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+import { useState, useEffect } from "react";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+  Heading
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+                    
+          <Response />
+          
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
+
+
+function Response() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <h1>{message}</h1>
+  );
+}
